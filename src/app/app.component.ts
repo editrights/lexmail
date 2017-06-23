231bad3e7650a018f88eda18a1b740b97f516a1b
ZdravkoVK <zdravkokirilov@yahoo.com>
2017-06-23 23:00:21 +0300

    animation tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,18 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { Router, NavigationStart } from '@angular/router';
 
-@Component({selector: 'app-root', templateUrl: './app.component.html', styleUrls: ['./app.component.scss']})
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
+})
 export class AppComponent {
+    private routerSubscription: any;
+    constructor(private router: Router) {
+        this.routerSubscription = router.events
+            .filter(event => event instanceof NavigationStart)
+            .subscribe((event: NavigationStart): void => {
+               
+            });
+    }
 }
--- src/app/authentication/auth-actions.service.ts
+++ src/app/authentication/auth-actions.service.ts
@@ -1,5 +1,4 @@
 import {Injectable} from '@angular/core';
-import auth0 from 'auth0-js';
 import {Http, Response} from '@angular/http';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
@@ -8,7 +7,6 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class AuthActionsService {
 
-  private webAuth = new auth0.WebAuth({clientID: 'mF5MQjxUy6YF1CtkSA2WUC7sSOUNO3vu', domain: 'zdravkovk.eu.auth0.com'});
   private login_url = 'https://zdravkovk.eu.auth0.com/authorize'
   constructor(private http : Http) {}
 
@@ -17,9 +15,6 @@ export class AuthActionsService {
     google: 'google-oauth2'
   }
   public login() : any {
-    // this   .webAuth   .popup   .authorize({     connection:
-    // this.providers.google,     responseType: 'token'   }, function (a, b) {
-    // console.log(a, b);   });
 
     this
       .http
